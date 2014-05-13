c4cf9e4100fd7ddc4ee2014feaf28061e9a8088b
Yaniv Bronhaim <ybronhei@redhat.com>
2014-05-13 05:32:45 +0000

    fix for host_ready?
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- app/models/concerns/ovirt_provision_plugin/host_extensions.rb
+++ app/models/concerns/ovirt_provision_plugin/host_extensions.rb
@@ -1,30 +1,43 @@
+require 'open3'
+
 module OvirtProvisionPlugin
   module HostExtensions
     extend ActiveSupport::Concern
 
     included do
       # execute callbacks
-      after_update :engine_callback
+      after_save :engine_callback
     end
 
     # create or overwrite instance methods...
     def engine_callback
+        logger.debug "hey im on engine_callback"
         if host_ready? && provisioned_recently? && ovirt_host?
+            exec_cmd("echo 'I am here'")
             # call ovirt
         end
     end
 
     def host_ready?
-        fail(Foreman::Exception, "Latest Puppet Run Contains Failures for Host: #{id}") if host.failed > 0
-        host.skipped == 0 && host.pending == 0
+        fail(Foreman::Exception, "Latest Puppet Run Contains Failures for Host: #{id}") if failed > 0
+        skipped == 0 && pending == 0
     end
     
     def provisioned_recently?
-        old.last_repost = nil && last_report != nil
+        last_report_was.blank? && last_report.blank?
     end
 
     def ovirt_host?
-	# ?
+	return true
     end
+
+    def exec_cmd(cmd)
+        stdin, stdout, stderr = Open3.popen3(cmd)
+        out=stdout.gets
+        err=stdout.gets
+        raise "ForemanPluginExecCmd: Could not execute command #{cmd}. Error: #{err}, Output: #{out}" unless $?.to_i == 0    
+        [ out, err ]
+    end
+
   end
 end
