4dbad4f93f3def55ac93f0f8613293e24b8a4235
Yaniv Bronhaim <ybronhei@redhat.com>
2014-05-27 13:26:34 +0000

    improvements
    Signed-off-by: Yaniv Bronhaim <ybronhei@redhat.com>

--- app/models/concerns/ovirt_provision_plugin/host_extensions.rb
+++ app/models/concerns/ovirt_provision_plugin/host_extensions.rb
@@ -3,48 +3,97 @@ module OvirtProvisionPlugin
     extend ActiveSupport::Concern
 
     included do
-      after_save :engine_callback
+        before_provision :ovirt_host_callback
     end
 
-    def engine_callback
-        if host_ready? && provisioned_recently? && ovirt_host?
-            cr = ComputeResource.find_by_name("ovirt-engine")
-            host_id = parameters.find_by_name("host_ovirt_id").value
-            logger.debug "OvirtProvisionPlugin:: Host #{host_id} as been provisioned. Sending approve request to #{cr.url}"
-            client = OVIRT::Client.new("#{cr.user}", "#{cr.password}", "#{cr.url}")
-
-            logger.debug "OvirtProvisionPlugin:: Sending approve "
-            client.approve_host("#{host_id}")
+    def get_ovirt_client
+        begin
+            cr = ComputeResource.find_by_id(parameters.find_by_name("compute_resource_id").value)
+            return OVIRT::Client.new("#{cr.user}", "#{cr.password}", "#{cr.url}")
+        rescue OVIRT::OvirtException
+            logger.debug "OvirtProvisionPlugin:: compute resource id was not found"
+            return false
+        else
+            logger.debug "OvirtProvisionPlugin:: error occured during get_ovirt_client"
+            return false
         end
     end
 
-    def reinstall_host
-        cr = ComputeResource.find_by_name("ovirt-engine")
-        host_id = parameters.find_by_name("host_ovirt_id").value
-        client = OVIRT::Client.new("#{cr.user}", "#{cr.password}", "#{cr.url}")
-        logger.debug "OvirtProvisionPlugin:: Sending approve "
-        client.reinstall_host("#{host_id}")
+    def get_ovirt_host
+        client = get_ovirt_client
+        if not client
+            logger.debug "OvirtProvisionPlugin:: couldn't get ovirt_host"
+            return ""
+        else
+            return client.host(get_ovirt_host_id)
+        end
     end
 
-    def approve_host
-        cr = ComputeResource.find_by_name("ovirt-engine")
-        host_id = parameters.find_by_name("host_ovirt_id").value
-        client = OVIRT::Client.new("#{cr.user}", "#{cr.password}", "#{cr.url}")
-        logger.debug "sending approve"
-        client.approve_host("#{host_id}")
+    def get_ovirt_host_name
+        h = get_ovirt_host
+        if not h
+            return ""
+        else
+            return h.name
+        end
+    end
+
+    def get_ovirt_host_id
+        begin
+            return parameters.find_by_name("host_ovirt_id").value
+        rescue OVIRT::OvirtException
+            logger.debug "OvirtProvisionPlugin:: host ovirt id was not found"
+            return false
+        else
+            logger.debug "OvirtProvisionPlugin:: error occured during get_ovirt_host_id"
+            return false
+        end
+    end
+
+    def is_ovirt_node?
+        is_ovirt_node = operatingsystem.name == "oVirt-Node" or operatingsystem.name == "RHEV-H"
+        if is_ovirt_node
+            logger.debug "OvirtProvisionPlugin:: Provisioned ovirt node host"
+            return true
+        end
+        return false
+    end
+
+    def ovirt_host_callback
+        logger.debug "OvirtProvisionPlugin:: Running provision callback.."
+        # TODO: check if host_ready? relevant to this condition
+        if ovirt_host? && status_installing? && (not is_ovirt_node?)
+            logger.debug "OvirtProvisionPlugin:: Running ovirt_host_callback on \"#{get_ovirt_host_name}\""
+            host_id = get_ovirt_host_id
+            client = get_ovirt_client
+            client.reinstall_host("#{host_id}")
+        end
     end
 
     def host_ready?
         fail(Foreman::Exception, "Latest Puppet Run Contains Failures for Host: #{id}") if failed > 0
-        skipped == 0 && pending == 0
-    end
-
-    def provisioned_recently?
-        last_report_was.blank? && last_report.blank?
+        if skipped == 0 && pending == 0
+            logger.debug "OvirtProvisionPlugin:: host ready"
+            return true
+        end
+        return false
     end
 
     def ovirt_host?
-        return (not parameters.find_by_name("host_ovirt_id").blank?)
+        if get_ovirt_host_id
+            logger.debug "OvirtProvisionPlugin:: host related to oVirt"
+            return true
+        end
+        return false
+    end
+
+    def status_installing?
+        h = get_ovirt_host
+        if h && h.status.strip == "installing_os"
+            logger.debug "OvirtProvisionPlugin:: host in status installing"
+            return true
+        end
+        return false
     end
   end
 end
