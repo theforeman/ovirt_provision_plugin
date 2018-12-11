29bdcd26acfe9a39bd948308574cfad3e91e825d
Moti Asayag <masayag@redhat.com>
2018-12-11 12:30:37 +0200

    Remove locale folder

--- locale/gemspec.rb
+++
@@ -1,2 +0,0 @@
-# Matches ovirt_provision_plugin.gemspec
-_("Ovirt provision plugin sends API request to oVirt management to reinstall host id after discovered hosts are first provisioned by oVirt engine (Using foreman provider integration).")
